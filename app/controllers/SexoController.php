94871861da42b74e7cda64d31f68da04e3ad4bd7
Marion Glatzle <imarionglatzlemina-wq>
2026-04-15 21:04:06 -0500

    Mejoras

--- app/controllers/SexoController.php
+++ app/controllers/SexoController.php
@@ -5,8 +5,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 // En SexoController.php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/eysphp/config/database.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/eysphp/app/models/Sexo.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/eysphp5aapple/config/database.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/eysphp5aapple/app/models/Sexo.php';
 
 
 
